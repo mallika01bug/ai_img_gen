4f8c163e92596d8b201d1f676d8a66d032369041
Mallika Srivastava <79866597+mallika01bug@users.noreply.github.com>
2026-05-05 02:25:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from huggingface_hub import InferenceClient
+from PIL import Image
+import datetime
+
+
+client=InferenceClient(token=os.genenv(HF_TO_KEN"))
+MODEL="stabilityai/stable-diffusion-xl-base-1.0"
+st.set_page_config(page_title="AI Image Generator", page_icon="🖼️")
+st.title("AI IMage Generator")
+st.write("Describe your imagination to maake it real...")
+prompt=st.text_input("describe your image...")
+
+
+if st.button("Generate"):
+    with st.spinner("AI is creating your image..."):
+        image=client.text_to_image(prompt,model=MODEL)
+        st.image(image,caption=prompt)
+        image.save("image.png")
+        st.success("image saved successfully")
