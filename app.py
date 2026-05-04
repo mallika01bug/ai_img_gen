ffa7f849b379d3d69a018eb24c6a3cf976e0111c
Mallika Srivastava <79866597+mallika01bug@users.noreply.github.com>
2026-05-05 03:06:07 +0530

    Fix token retrieval in InferenceClient initialization

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 import os
 
 
-client=InferenceClient(token=os.getenv(HF_TOKEN))
+client=InferenceClient(token=os.getenv("HF_TOKEN"))
 MODEL="stabilityai/stable-diffusion-xl-base-1.0"
 st.set_page_config(page_title="AI Image Generator", page_icon="🖼️")
 st.title("AI IMage Generator")
